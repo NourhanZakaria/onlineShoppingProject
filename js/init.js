d8fe04ba26dacbe097ec08529cc146ae7ab638f5
momenkamal221 <momenkamal221@gmail.com>
2023-08-13 20:14:52 +0300

    asdl

---
+++ js/init.js
@@ -0,0 +1,12 @@
+localStorage.setItem('users','["admin","shimaa"]')
+localStorage.setItem('currentUser','admin')
+localStorage.setItem('carts','{"admin":[1,4],"shimaa":[]}')
+localStorage.setItem('products',
+	`[
+		{"name":"Thermo Ball Etip Gloves", "price":"$ 45,743", "pic":"img/xpopular1.png"},
+		{"name":"Thermo Ball Etip Gloves", "price":"$ 45,743", "pic":"img/xpopular2.png"},
+		{"name":"Thermo Ball Etip Gloves", "price":"$ 45,743", "pic":"img/xpopular3.png"},
+		{"name":"Thermo Ball Etip Gloves", "price":"$ 45,743", "pic":"img/xpopular4.png"},
+		{"name":"Thermo Ball Etip Gloves", "price":"$ 45,743", "pic":"img/xpopular5.png"},
+		{"name":"Thermo Ball Etip Gloves", "price":"$ 45,743", "pic":"img/xpopular6.png"}]`
+)
--- js/main.js
+++ js/main.js
@@ -1,16 +1,39 @@
-const currentUser='momen';
-const users=[
-	{
-		name:'momen',
-		pw:1234,
-		cart:[
-			1,2,3
-		]
+//global
+function toggleProductInCart(id) {
+	let carts = localStorage.getItem("carts");
+	let currentUser = localStorage.getItem("currentUser");
+	carts = JSON.parse(carts);
+	if (carts[currentUser].includes(id)) {
+		carts[currentUser] = carts[currentUser].filter((x) => x !== id);
+		msgBox(`product has been "removed" from your cart`);
+	} else {
+		carts[currentUser].push(id);
+		msgBox(`product has been "added" to your cart`);
 	}
-]
-const products=[
-{
-	id:1,
-	price:20,
+	console.log(JSON.stringify(carts));
+	localStorage.setItem("carts", JSON.stringify(carts));
 }
-]
+function isInCart(id) {
+	let carts = localStorage.getItem("carts");
+	let currentUser = localStorage.getItem("currentUser");
+	carts = JSON.parse(carts);
+	if (carts[currentUser].includes(id)) {
+		return true;
+	}
+	return false;
+}
+
+let hideMsgTimeout;//dont move this
+function msgBox(msg) {
+	clearTimeout(hideMsgTimeout);
+	const msgDiv = document.getElementById("msgBox");
+	setTimeout(() => msgDiv.classList.remove("msg-box_show"), 50);
+	setTimeout(() => {
+		msgDiv.innerText = msg;
+		msgDiv.classList.add("msg-box_show");
+		hideMsgTimeout = setTimeout(
+			() => msgDiv.classList.remove("msg-box_show"),
+			5000
+		);
+	}, 300);
+}
